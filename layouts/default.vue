75254b81928bf01390eb6523661eb6c3e4e80f77
Alex-G-Mommertz <alexmomm.23@gmail.com>
2021-12-20 16:54:55 +0100

    init

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app dark>
+
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <PopForm />
+    </v-app-bar>
+    <v-main>
+      <v-container
+      fluid>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
+import PopForm from "../components/form.vue"
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    PopForm
+ } 
+}
+</script>
